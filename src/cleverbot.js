00db565c3134c472f51305ba2befb7ee90cf56ce
Matt <ballofmatts@gmail.com>
2017-06-06 22:17:01 -0500

    Change behavior to look for keyword, add random emoji faces
    Small life improvements to make my friends giggle

--- src/cleverbot.js
+++ src/cleverbot.js
@@ -1,5 +1,8 @@
 // Description:
 //   Turns your bot into a snarky little shit
+//   Responds when it sees a set keyword (crow in this example). Set keyword is stripped out of sentence before being sent to cleverbot.
+//   Replaces cleverbot's use of the word 'computer' or 'robot' with 'pingas', since it seems to fixate on that topic a lot.
+//   Adds random preset emoji face at the end of responses.
 //
 // Dependencies:
 //   "cleverbot-node": "0.3.3"
@@ -8,10 +11,11 @@
 //   Environment variable CLEVERBOT_API_KEY needs to be set
 //
 // Commands:
-//   hubot c <input>
+//   any sentence with the word 'crow'
 //
 // Author:
 //   lucasvanhalst
+//   ballofmatts
 var Cleverbot = require("cleverbot-node");
 
 module.exports = function(robot) {
@@ -20,11 +24,13 @@ module.exports = function(robot) {
 	if (!apiKey){
 		throw Error('You need to set the CLEVERBOT_API_KEY environment variable.')
 	}
+	var crowFaces = Array(':crowbot:',':crowbot1:',':crowbot2:',':crowbot3:',':crowbot4:',':crowbot5:',':crowbot6:',':crowbot7:',':crowbot8:',':crowbot9:');
 	cleverbot.configure({botapi: apiKey})
-	return robot.respond(/c (.*)/i, function(msg) {
-		var data = msg.match[1].trim();
+	return robot.hear(/((.*\bcrow\b.*))/i, function(msg) {
+		var data = msg.match[1].trim().replace(/\bcrow\b/gi,'');
+		var crowFace = crowFaces[Math.floor(Math.random()*crowFaces.length)];
 		cleverbot.write(data, function(response){
-			msg.send(response.output);
+			msg.send(response.output.replace(/\bcomputer|robot\b/gi,'pingas') + crowFace);
 		});
 	});
 };
